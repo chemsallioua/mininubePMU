bc1dba93f16f57aced5790cb0c2050090072d300
c_allioua <callioua@minsait.com>
2023-05-26 19:48:44 +0200

    init commit

---
+++ node-gateway-simulator.py
@@ -0,0 +1,138 @@
+import requests, json
+import base64, struct, math, time
+
+class RequestException(Exception):
+    pass
+
+class NodeGatewaySimulator:
+    def __init__(self, url = "http://127.0.0.1:5000"):
+        self.url = url
+
+    def post_configure(self, configuration, endpoint = "/configure"):
+
+        url = self.url + endpoint
+        # Send the POST request with the configuration
+        response = requests.post(url, json={"configuration": configuration})
+
+        # Check the response
+        if response.status_code == 200:
+            # Successful response
+            data = response.json()
+            status = data["status"]
+            print(f"Configuration status: {status}")
+        else:
+            # Error response
+            raise RequestException(response.text)
+
+
+    def get_estimate(self, data_frame, endpoint = "/estimate"):
+
+        url = self.url + endpoint
+        # Send the POST request with the data frame
+        message= {"data_frame": data_frame}
+
+        json_string = json.dumps(message)
+        payload_size = len(json_string)
+        print("Payload size:", payload_size, "bytes")
+
+        response = requests.post(url, json=message)
+
+        # Check the response
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise RequestException(response.text)
+
+    @staticmethod
+    def get_encoded_signal(nominal_freq = 50, amplitude = 1.0 ,phase = 0, frequency = 51.0, sampling_rate = 25600, n_cycles = 4):
+
+        num_samples = n_cycles*sampling_rate//nominal_freq
+        samples = []
+
+        for i in range(num_samples):
+            time = i / sampling_rate
+            sample = amplitude * math.sin(2 * math.pi * frequency * time + phase)
+            samples.append(sample)
+
+        # Convert the samples to a bytes object using double precision
+        byte_data = b"".join(struct.pack("d", sample) for sample in samples)
+
+        # Encode the bytes data to base64
+        return base64.b64encode(byte_data).decode("utf-8")
+
+
+if __name__ == "__main__":
+
+    # Create a NodeGatewaySimulator object
+    node_gateway = NodeGatewaySimulator(url = "http://localhost:5000")
+
+    n_cycles = 4
+    sample_rate = 25600//64
+    nominal_freq = 50
+    NUM_CHANNELS = 4
+
+    # Configure PMU
+    configuration = {
+        "signal": {
+            "n_cycles": n_cycles,
+            "sample_rate": sample_rate,
+            "nominal_freq": nominal_freq
+        },
+        "synchrophasor": {
+            "frame_rate": 50,
+            "number_of_dft_bins": 11,
+            "ipdft_iterations": 3,
+            "iter_e_ipdft_enable": 1,
+            "iter_e_ipdft_iterations": 10,
+            "interference_threshold": 0.0033
+        },
+        "rocof": {
+            "threshold_1": 3,
+            "threshold_2": 25,
+            "threshold_3": 0.035,
+            "low_pass_filter_1": 0.5913,
+            "low_pass_filter_2": 0.2043,
+            "low_pass_filter_3": 0.2043
+        }
+    }
+    try:
+        node_gateway.post_configure(configuration)
+    except Exception as e:   
+        print(e)
+    
+    data_frame = {
+        "timestamp":
+            {
+            "SOC": 123456789,
+            "FRACSEC": 0,
+            "timebase": 1000000
+            },
+    }
+
+    # Add the encoded signal to the data frame
+    data_frame["channels"] = []
+    
+    for i in range(1, NUM_CHANNELS):
+        # Get the encoded signal
+        signal_frequency = 50.0 + i
+        encoded_signal = node_gateway.get_encoded_signal(nominal_freq = nominal_freq, frequency = signal_frequency, sampling_rate = sample_rate, n_cycles = n_cycles)
+
+        data_frame["channels"].append({
+            "channel_number": i,
+            "payload": encoded_signal
+        })
+
+    # Get the estimate
+    try:
+        for i in range(0, 10):
+            start = time.time()
+            estimate = node_gateway.get_estimate(data_frame)
+            end = time.time()
+            print("Time elapsed:", end - start, " seconds")
+        print(estimate)
+    except Exception as e:
+        print(e)
+    
+    
+
+    
---
+++ pmu-rest-api.py
@@ -0,0 +1,168 @@
+from flask import Flask, request, abort
+from flask_restful import Api, Resource
+import json
+from jsonschema import validate, ValidationError
+from pmu_estimator import PMUEstimator, EstimatorConfig
+import base64
+import struct
+
+app = Flask(__name__)
+api = Api(app)
+
+# initialize pmu estimator object
+synchestim = PMUEstimator()
+
+class Configure(Resource):
+
+    configuration_schema = {
+        "type": "object",
+        "properties": {
+            "signal": {
+                "type": "object",
+                "properties": {
+                    "n_cycles": {"type": "integer"},
+                    "sample_rate": {"type": "integer"},
+                    "nominal_freq": {"type": "integer"}
+                },
+                "required": ["n_cycles", "sample_rate", "nominal_freq"]
+            },
+            "synchrophasor": {
+                "type": "object",
+                "properties": {
+                    "frame_rate": {"type": "integer"},
+                    "number_of_dft_bins": {"type": "integer"},
+                    "ipdft_iterations": {"type": "integer"},
+                    "iter_e_ipdft_enable": {"type": "integer"},
+                    "iter_e_ipdft_iterations": {"type": "integer"},
+                    "interference_threshold": {"type": "number"}
+                },
+                "required": ["frame_rate", "number_of_dft_bins", "ipdft_iterations", "iter_e_ipdft_enable", "iter_e_ipdft_iterations", "interference_threshold"]
+            },
+            "rocof": {
+                "type": "object",
+                "properties": {
+                    "threshold_1": {"type": "number"},
+                    "threshold_2": {"type": "number"},
+                    "threshold_3": {"type": "number"},
+                    "low_pass_filter_1": {"type": "number"},
+                    "low_pass_filter_2": {"type": "number"},
+                    "low_pass_filter_3": {"type": "number"}
+                },
+                "required": ["threshold_1", "threshold_2", "threshold_3", "low_pass_filter_1", "low_pass_filter_2", "low_pass_filter_3"]
+            }
+        },
+        "required": ["signal", "synchrophasor", "rocof"]
+    }
+
+    
+    
+    def post(self):
+        data = request.get_json()
+
+        if not data or 'configuration' not in data:
+            abort(400)
+
+        configuration = data['configuration']
+
+        try:
+            validate(configuration, self.configuration_schema)
+        except ValidationError as e:
+            abort(400)
+
+        # process_configuration(configuration)
+        synchestim_config = EstimatorConfig(
+            n_cycles = configuration['signal']['n_cycles'],
+            fs = configuration['signal']['sample_rate'],
+            f0 = configuration['signal']['nominal_freq'],
+            frame_rate = configuration['synchrophasor']['frame_rate'],
+            n_bins = configuration['synchrophasor']['number_of_dft_bins'],
+            P = configuration['synchrophasor']['ipdft_iterations'],
+            iter_eipdft = configuration['synchrophasor']['iter_e_ipdft_enable'],
+            Q = configuration['synchrophasor']['iter_e_ipdft_iterations'],
+            interf_trig = configuration['synchrophasor']['interference_threshold'],
+            rocof_thresh = [configuration['rocof']['threshold_1'], configuration['rocof']['threshold_2'], configuration['rocof']['threshold_3']],
+            rocof_low_pass_coeffs= [configuration['rocof']['low_pass_filter_1'] , configuration['rocof']['low_pass_filter_2'], configuration['rocof']['low_pass_filter_3']]
+        )
+        synchestim.deinit()
+        if (synchestim.configure_from_class(synchestim_config) != 0):   
+            abort(500)
+
+        return {"status": "Successfully Configured PMU Estimator"}
+
+class Estimate(Resource):
+
+    data_frame_schema = {
+        "type": "object",
+        "properties": {
+            "timestamp": { 
+                "type": "object",
+                "properties": {
+                    "SOC": {"type": "integer", "minimum": 0},
+                    "FRACSEC": {"type": "integer", "minimum": 0},
+                    "timebase": {"type": "integer", "minimum": 0}
+                },
+                "required": ["SOC", "FRACSEC", "timebase"]
+            },
+            "channels": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "channel_number": {"type": "integer"},
+                        "payload": {"type": "string"}
+                    },
+                    "required": ["channel_number", "payload"]
+                }
+            }
+        },
+        "required": ["timestamp", "channels"]
+    }
+
+    def post(self):
+        data = request.get_json()
+
+        if not data or 'data_frame' not in data:
+            abort(400)
+
+        data_frame = data['data_frame']
+
+        # try:
+        #     validate(data_frame, self.data_frame_schema)
+        # except ValidationError as e:
+        #     print(e)
+        #     abort(400)
+        
+        # getting the midwindow fracsec
+        timebase =data_frame['timestamp']['timebase']
+
+        if timebase != 0:
+            mid_window_fracsec = data_frame['timestamp']['FRACSEC']/timebase
+        else:
+            mid_window_fracsec = 0
+
+        # frame = {}
+        # for channel in data_frame['channels']:
+        #     decoded_data = base64.b64decode(channel['payload'])
+        #     input_signal_window = []
+        #     for i in range(0, len(decoded_data), 8):
+        #         sample = struct.unpack("d", decoded_data[i:i+8])[0]
+        #         input_signal_window.append(sample)    
+        #     estimated_frame = synchestim.estimate(input_signal_window, mid_window_fracsec)
+        #     if estimated_frame is None:
+        #         abort(500)
+
+        #     frame["channel_" + str(channel['channel_number'])] = estimated_frame
+        frame = 1000
+        return {"frame": frame}
+
+    def perform_estimate(data_frame):
+        # Perform the necessary calculations on the data frame to get the estimate
+        # For this example, we'll return a fixed value
+        return 2048
+
+api.add_resource(Estimate, "/estimate")
+api.add_resource(Configure, "/configure")
+
+if __name__ == "__main__":
+
+    app.run(debug=True, threaded=True, host='127.0.0.1', port=5000)
